68450ffcafd3741a2d3159ea4991199f009e1461
KesterDaniel <kesterdan17@gmail.com>
2023-01-12 12:45:44 +0100

    init commit

---
+++ menu.py
@@ -0,0 +1,25 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 10
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24
+        },
+        "cost": 2.5
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24
+        },
+        "cost": 2.5
+    }
+}
