66777dec5537649235b46e54d27c99e515310473
KesterDaniel <kesterdan17@gmail.com>
2023-01-12 13:17:42 +0100

    checks for availaibility of ingredients

--- app.py
+++ app.py
@@ -7,11 +7,23 @@ init_Stats = {
     "Money": 0
 }
 
+
+def check_resource(order):
+    """Checks if there is enough ingredients to process the order"""
+    ingredients = MENU[order]["ingredients"]
+    for item in ingredients:
+        if init_Stats[item] - ingredients[item] < 0:
+            return f"Sorry, There is not enough {item}"
+        else:
+            return "Proceed"
+
+
 user_input = input("What would you like? (espresso/latte/cappuccino): ")
 
 if user_input == "report":
     for stat in init_Stats:
         print(f"{stat}: {init_Stats[stat]}")
 else:
-    pass
+    print(check_resource(user_input))
+
 
