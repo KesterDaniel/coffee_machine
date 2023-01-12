d94205c1a994ca125019fd35a029f137ef05fe77
KesterDaniel <kesterdan17@gmail.com>
2023-01-12 21:23:57 +0100

    calculate total cost of order

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from menu import MENU
 
 init_Stats = {
-    "Water": 300,
-    "Milk": 200,
-    "Coffee": 100,
-    "Money": 0
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
 }
 
 
@@ -24,6 +24,18 @@ if user_input == "report":
     for stat in init_Stats:
         print(f"{stat}: {init_Stats[stat]}")
 else:
-    print(check_resource(user_input))
+    availability = check_resource(user_input)
+    if availability != "Proceed":
+        print(availability)
+    else:
+        print("Please insert coins")
+        quarters = float(input("How many quarters: ")) * 0.25
+        dimes = float(input("How many dimes: ")) * 0.10
+        nickels = float(input("How many nickel: ")) * 0.05
+        pennies = float(input("How many pennies: ")) * 0.01
+        cost = (quarters + dimes + nickels + pennies)
+        total_cost = "{:.2f}".format(cost)
+        print(total_cost)
+
 
 
